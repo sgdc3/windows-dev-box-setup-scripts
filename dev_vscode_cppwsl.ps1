35045071fd74b779a021e6a13be3e9892930725b
Gabriele C <sgdc3.mail@gmail.com>
2019-04-17 12:11:09 +0200

    Create dev_vscode_cppwsl.ps1

---
+++ dev_vscode_cppwsl.ps1
@@ -0,0 +1,40 @@
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Setting up Windows ---
+executeScript "SystemConfiguration.ps1";
+executeScript "FileExplorerSettings.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "CommonDevTools.ps1";
+executeScript "WSL.ps1";
+
+Update-SessionEnvironment
+
+write-host "Installing tools inside the WSL distro..."
+Ubuntu1804 run apt install build-essential -y
+write-host "Finished installing tools inside the WSL distro"
+
+write-host "Installing VSCode extensions..."
+code --install-extension ms-vscode.cpptools
+write-host "Finished installing VSCode extensions"
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
